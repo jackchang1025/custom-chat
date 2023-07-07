41efa21a783fe1f9afc39d1d44deb473daa82473
微加加的朋友 <shadowmatthew1025@gmail.com>
2023-07-07 18:11:50 +0800

    init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class HomeController extends Controller
+{
+    public function index(): View|\Illuminate\Foundation\Application|Factory|Application
+    {
+        return view('index');
+    }
+}
